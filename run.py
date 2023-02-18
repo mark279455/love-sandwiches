53576cb5507c3d21b6f70a57d5e7b527f602f560
mark279455 <mark279455@ntlworld.com>
2023-02-18 12:21:17 +0000

    add user request loop to request data from user until it is valid

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -17,19 +17,18 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 # print(data)
 
 
-
-
 def get_sales_data():
     """
     Get sales figures from the user
     """
-    print("Please enter sales dta from the last market.")
-    print("Should be 6 comma delimited numbers - 10,20,30,40,50,60\n")
-    data_str = input("Enter your data here: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
-
-
+    while True:
+        print("Please enter sales dta from the last market.")
+        print("Should be 6 comma delimited numbers - 10,20,30,40,50,60\n")
+        data_str = input("Enter your data here: ")
+        sales_data = data_str.split(",")
+        if validate_data(sales_data):
+            break
+    return sales_data
 
 
 def validate_data(data):
@@ -38,11 +37,14 @@ def validate_data(data):
     """
     print(data)
     try:
+        [int(num) for num in data]
         if len(data) != 6:
             raise ValueError(
                 f"6 values required - you gave {len(data)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again\n")
+        return False
+    return True
 
-get_sales_data()
+data = get_sales_data()
