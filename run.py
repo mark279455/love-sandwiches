67c719449458867e842b2bfe8ec6a4c649b5ddc3
mark279455 <mark279455@ntlworld.com>
2023-02-18 11:57:29 +0000

    create validata_data function to veryfy length of input to be 6 and all numbers

--- run.py
+++ run.py
@@ -16,6 +16,9 @@ SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 # data = sales.get_all_values()
 # print(data)
 
+
+
+
 def get_sales_data():
     """
     Get sales figures from the user
@@ -23,9 +26,23 @@ def get_sales_data():
     print("Please enter sales dta from the last market.")
     print("Should be 6 comma delimited numbers - 10,20,30,40,50,60\n")
     data_str = input("Enter your data here: ")
-    print(f"Data provided: [{data_str}]")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+
+
+
+def validate_data(data):
+    """
+    checks for 6 element array of numbers
+    """
+    print(data)
+    try:
+        if len(data) != 6:
+            raise ValueError(
+                f"6 values required - you gave {len(data)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again\n")
 
 get_sales_data()
-
-
-    
