c7ffc033278491bfb937a7669ae239462879d687
mark279455 <mark279455@ntlworld.com>
2023-02-19 11:20:58 +0000

    created calculate_surplus_data fuction and retrived stck numbers from worksheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -47,16 +48,39 @@ def validate_data(data):
         return False
     return True
 
+
 def update_sales_worksheet(data):
     """
     update sales worksheet, add new row with the list data provided
     """
     print("Updating sales worksheet")
-    sales_worksheet=SHEET.worksheet("sales")
+    sales_worksheet = SHEET.worksheet("sales")
     sales_worksheet.append_row(data)
     print("Update success\n")
 
 
-data = get_sales_data()
-data = [int(num) for num in data]
-update_sales_worksheet(data)
+def calculate_surplus_data(sales_row):
+    """
+    comapere sales with stockand calculate surplus for each type
+
+    surplus = stock - sales
+    posiive result = waste
+    negative result = extra made when stock was exhausted
+    """
+    print("Caloculating surplus data...")
+    stock = SHEET.worksheet("stock").get_all_values()
+    pprint(stock)
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    main program flow
+    """
+    data = get_sales_data()
+    data = [int(num) for num in data]
+    update_sales_worksheet(data)
+    calculate_surplus_data(data)
+
+main()
